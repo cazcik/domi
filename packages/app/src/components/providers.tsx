e7ac927dd9532cc434eee61eec3e2a2a0fed7c4c
Zac Wojcik <zac.wojcik@gmail.com>
2024-08-27 15:31:45 -0400

    don't use analytics in dev

--- packages/app/src/components/providers.tsx
+++ packages/app/src/components/providers.tsx
@@ -1,14 +1,17 @@
 import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
-if (typeof window !== "undefined") {
-  posthog.init("phc_1Ogcp16thrs0Rff8hcueMLAjSF3paFAbrX0cAmotrsu", {
-    api_host: "https://circles.usedomi.com",
-    person_profiles: "identified_only",
-    capture_pageview: false,
-  });
-}
-
 export function PHProvider({ children }: { children?: React.ReactNode }) {
-  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  if (import.meta.env.MODE === "production") {
+    if (typeof window !== "undefined") {
+      posthog.init("phc_1Ogcp16thrs0Rff8hcueMLAjSF3paFAbrX0cAmotrsu", {
+        api_host: "https://circles.usedomi.com",
+        person_profiles: "identified_only",
+      });
+    }
+
+    return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+  } else {
+    return <>{children}</>;
+  }
 }
--- packages/app/src/routes/root.tsx
+++ packages/app/src/routes/root.tsx
@@ -1,12 +1,9 @@
 import { Outlet } from "react-router-dom";
 
-import PostHogPageView from "../components/pageview";
-
 export default function Root() {
   return (
     <div id="root-layout">
       <Outlet />
-      <PostHogPageView />
     </div>
   );
 }
