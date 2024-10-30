c137e108a0140a1a07c08ed1e1495ec329253e46
Zac Wojcik <zac.wojcik@gmail.com>
2024-10-30 11:13:14 -0400

    small ups

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,10 +8,10 @@ export default function NotFound() {
   return (
     <div className="flex min-h-dvh flex-col">
       <div className="m-auto w-full max-w-xl">
-        <div className="px-6">
-          <h1 className="text-xl font-bold text-black">404</h1>
-          <p className="text-neutral-600">
-            oops, looks like that page isn't here.
+        <div className="flex flex-col px-6">
+          <h1 className="text-xl font-bold text-black lg:text-2xl">404.</h1>
+          <p className="text-lg text-neutral-500 lg:mt-1 lg:text-xl">
+            sorry, that page doesn't seem to exist.
           </p>
         </div>
       </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,11 @@ export default function IndexPage() {
   return (
     <div className="flex min-h-dvh flex-col">
       <div className="m-auto w-full max-w-xl">
-        <div className="flex items-center gap-x-1 px-6 text-black">
+        <div className="flex flex-col px-6">
           <h1 className="text-xl font-bold text-black lg:text-2xl">domi.</h1>
-        </div>
-        <div className="flex flex-col px-6 text-lg text-neutral-500 lg:mt-1 lg:text-xl">
-          <p>the friendly way to homeschool.</p>
+          <p className="text-lg text-neutral-500 lg:mt-1 lg:text-xl">
+            the friendly way to homeschool.
+          </p>
         </div>
       </div>
     </div>
