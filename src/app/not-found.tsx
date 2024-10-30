a617d746fac65f9ccee9bfe9e32a5e9f8fabce02
Zac Wojcik <zac.wojcik@gmail.com>
2024-10-30 09:16:31 -0400

    back to workers + static

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,19 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404",
+};
+
 export default function NotFound() {
   return (
-    <div className="flex flex-col space-y-7">
-      <div>
-        <h1 className="text-xl font-bold text-black">404</h1>
-        <p className="text-neutral-600">
-          oops, looks like that page isn't here.
-        </p>
+    <div className="flex min-h-dvh flex-col">
+      <div className="m-auto w-full max-w-xl">
+        <div className="px-6">
+          <h1 className="text-xl font-bold text-black">404</h1>
+          <p className="text-neutral-600">
+            oops, looks like that page isn't here.
+          </p>
+        </div>
       </div>
     </div>
   );
