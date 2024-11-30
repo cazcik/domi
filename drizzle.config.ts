c45259c158bb03d6d14370e27cd3d0c043b6208b
Zac Wojcik <zac.wojcik@gmail.com>
2024-11-29 21:49:24 -0500

    frsh

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  schema: "./app/db.ts",
-  dialect: "postgresql",
-  dbCredentials: {
-    url: process.env.DATABASE_URL!,
-  },
-});
