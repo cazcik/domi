b4fcb555a9a2f5735cb5ad2fc05d982a21514ea3
Zac Wojcik <zac.wojcik@gmail.com>
2025-02-19 21:19:24 -0500

    see what i mean

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import { Inter } from "next/font/google";
 import type { Metadata, Viewport } from "next";
 import { Analytics } from "@vercel/analytics/react";
-import { AuthKitProvider } from "@workos-inc/authkit-nextjs/components";
 
 import "./globals.css";
 
@@ -59,7 +58,7 @@ export default function RootLayout({
       className={`${inter.variable} antialiased smooth-scroll bg-white dark:bg-black text-black dark:text-white`}
     >
       <body>
-        <AuthKitProvider>{children}</AuthKitProvider>
+        {children}
         <Analytics />
       </body>
     </html>
