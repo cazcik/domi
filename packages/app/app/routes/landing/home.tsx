a968e5caae1ef8e95d4cd82842b9da112acc5957
Zac Wojcik <zac.wojcik@gmail.com>
2025-04-11 17:55:53 -0400

    clean

--- packages/app/app/routes/landing/home.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="flex min-h-dvh">
-      <div className="m-auto">
-        <h1 className="text-black dark:text-white">DOMI</h1>
-      </div>
-    </div>
-  );
-}
