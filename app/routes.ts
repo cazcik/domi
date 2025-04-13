59c8a8fe753ac655a430748512c9b06935144a39
Zac Wojcik <zac.wojcik@gmail.com>
2025-04-13 10:44:56 -0400

    clean

--- app/routes.ts
+++
@@ -1,5 +0,0 @@
-import { type RouteConfig, index, layout } from "@react-router/dev/routes";
-
-export default [
-  layout("routes/marketing/layout.tsx", [index("routes/marketing/home.tsx")]),
-] satisfies RouteConfig;
